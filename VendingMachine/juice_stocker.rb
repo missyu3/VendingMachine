775c4798f6ee77bce8a5cf84217773e283701b7d
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-04-13 20:15:54 +0900

    test

--- VendingMachine/juice_stocker.rb
+++ VendingMachine/juice_stocker.rb
@@ -14,37 +14,35 @@ class JuiceStocker < ErrorMessage
         drink = change_hash(name, price: price)
         @stock_info.merge!(drink)
       end
-      @error.dequeue
+      dequeue
     end
 
     def add_drink(name,stock_count)
       if @stock_info.has_key?(name.intern)
         @stock_info[name.intern][1] += stock_count
       else
-        @error.set("initialize_drinkを先に実行してください")
+        set("initialize_drinkを先に実行してください")
       end
-      @error.dequeue
+      dequeue
     end
 
     def set_price(name,price)
       if @stock_info.has_key?(name.intern)
         @stock_info[name.intern][0] = price        
       else
-        @error.set("initialize_drinkを先に実行してください")
+        set("initialize_drinkを先に実行してください")
       end
-      @error.dequeue
+      dequeue
     end
 
     def pull_stock(name)
         @stock_info[name.intern][1] -= 1
     end
     
-    private
     def change_hash(name, price: 0, stock_count: 0)
-      puts "0"
       drink = Hash.new
       drink[name.intern] = [price, stock_count]
-      puts "1"
+      drink
     end
 end
 
